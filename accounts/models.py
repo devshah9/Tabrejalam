b873b6b973deabb883e31c7aecaccfcb3228eb77
dev <devshah972@gmail.com>
2022-08-17 17:01:29 +0530

    migration fix ..

--- accounts/models.py
+++ accounts/models.py
@@ -6,7 +6,20 @@ from django.contrib.auth.models import AbstractUser, AbstractBaseUser, Permissio
 
 
 class UserAccountManager(BaseUserManager):
-    def create_user(self, email, name, password=None):
+
+
+    def create_superuser(self, email, name, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email=email,name=name, password= password, **extra_fields)
+    def create_user(self, email, name, password=None, **extra_fields):
+        print(22, extra_fields)
         if not email:
             raise ValueError('Users must have an email address')
 
@@ -24,6 +37,7 @@ class UserAccount(AbstractUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
+    username = None
 
     objects = UserAccountManager()
 
