83dff35e3c3df5aeaaf8f07cc7e8f697bebf8607
dev <devshah972@gmail.com>
2022-08-23 02:59:58 +0530

    .

--- main/asgi.py
+++ main/asgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-
+import django
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+
+django.setup()
 
 application = get_asgi_application()
