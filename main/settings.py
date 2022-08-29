f2cf3644e37e1ff8c9ac78fd55a9fcc2f789f2a9
dev <devshah972@gmail.com>
2022-08-29 10:49:27 +0530

    small bug fix

--- main/settings.py
+++ main/settings.py
@@ -144,9 +144,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': {
+    'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
-    },
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
