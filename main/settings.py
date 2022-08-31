34acbd9f2714b862bc65851847b4b178cccbf93d
dev <devshah972@gmail.com>
2022-08-31 14:52:37 +0530

    .

--- main/settings.py
+++ main/settings.py
@@ -191,7 +191,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'tradingpipe960@gmail.com' 
-EMAIL_HOST_PASSWORD = 'TABREJALAM'
+EMAIL_HOST_PASSWORD = 'ettfgszhokcfmbjx'
 EMAIL_USE_TLS = True
 
 
